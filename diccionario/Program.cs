1e1898dbebe2524bc80ab2fbc96eb611f0378420
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-03 19:29:04 -0600

    Agregar archivos de proyecto.

---
+++ diccionario/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace diccionario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, (string nombre, int precio, int peso)> producto = new Dictionary<int, (string, int, int)>();
+            int opcion;
+            do
+            {
+                Console.WriteLine("1. agregar producto");
+                Console.WriteLine("2. modificar ");
+                Console.WriteLine("3. eliminar");
+                Console.WriteLine("4. ver");
+                int.TryParse(Console.ReadLine(), out opcion);
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("ingrese id de producto");
+                        int.TryParse(Console.ReadLine(), out int id);
+                        if (producto.ContainsKey(id))
+                        {
+                         
+                        }
+                        Console.WriteLine("ingrese nombre");
+                        nombre
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (opcion != 0);
+        }
+    }
+}
